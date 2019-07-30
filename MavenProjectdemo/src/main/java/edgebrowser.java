fcc80d24b3b2f3268286701cc83de654846494e1
tiju thomas <tiju thomas@192.168.0.3>
2019-07-30 18:14:47 +0530

    # WARNING: head commit changed in the meantime
    Test Commit

--- MavenProjectdemo/src/main/java/edgebrowser.java
+++ MavenProjectdemo/src/main/java/edgebrowser.java
@@ -1,4 +1,7 @@
 
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.testng.annotations.Test;
@@ -17,7 +20,12 @@ public class edgebrowser {
 
 	@Test(priority=2)
 	public void openEdgeBrowser() {
-		driver.navigate().to("http://www.google.com");
+		driver.navigate().to("https://apartmentadda.com/user/security.php?is_adda_io=0#/visitor/visitor-in");
+		driver.findElement(By.name("email")).sendKeys("thomas@3five8.com");
+	    driver.findElement(By.name("password")).sendKeys("adda12345");
+	    driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+	    //click on login button
+	    driver.findElement(By.id("submit_login")).click();
 	}
 	
 	@Test(priority=3)

437f0a57b78664eb1987edaf68b17df030e1462d
Tiju Thomas <thomas@3five8.com>
2019-08-09 16:20:46 +0530

    test commit

--- MavenProjectdemo/src/main/java/edgebrowser.java
+++ MavenProjectdemo/src/main/java/edgebrowser.java
@@ -3,8 +3,8 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-//import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.Test;
 import org.openqa.selenium.chrome.ChromeOptions;
 
@@ -22,20 +22,53 @@ public class edgebrowser {
 		driver = new ChromeDriver(options);
 	}
 
-	@Test(priority=2)
-	public void openChromeBrowser() {
+	 @Test(priority=1)
+     public void openADDAGK() {
 		driver.navigate().to("https://apartmentadda.com/user/security.php?is_adda_io=0#/visitor/visitor-in");
 		driver.findElement(By.name("email")).sendKeys("thomas@3five8.com");
 	    driver.findElement(By.name("password")).sendKeys("adda12345");
 	    driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
-	    //click on login button
 	    driver.findElement(By.id("submit_login")).click();
-	}
-	
-	//@Test(priority=3)
-	public void closeDriver() {
+	    
+	    }
+     @Test(priority=2)
+     public void CheckIn() throws InterruptedException {
+	    driver.findElement(By.xpath("//ng-multiselect-dropdown[@id='unit_number']")).click();
+	    driver.findElement(By.xpath("//div[contains(text(),'Block 1-002')]")).click();
+	    driver.findElement(By.xpath("//tr"));	
+	    driver.findElement(By.xpath("//button[@type='button'][contains(text(),'CHECK IN')]")).click();
+	    Thread.sleep(5000);
+     driver.findElement(By.xpath("//ng-multiselect-dropdown[@id='reason']")).click();
+	    driver.findElement(By.xpath("//div[contains(text(),'Personal')]")).click();
+	    driver.findElement(By.xpath("//button[@name='save_visiter']")).click();
+	    Thread.sleep(5000);
+	    
+     }
+	   
+	    @Test(priority=3)
+	    public void CheckOut() throws InterruptedException {
+	    driver.findElement(By.xpath("//a[@href='#/visitor/visitor-out']")).click();
+	    driver.findElement(By.xpath("//button[@type='button' and contains(., 'CHECK OUT')]")).click();
+	    Thread.sleep(5000);
+	    }
+	    @Test(priority=4)
+	    public void staffcheckin() throws InterruptedException {
+	    driver.findElement(By.xpath("//p[contains(text(),'Staff')]")).click();
+	    driver.findElement(By.xpath("//button[@type='button'][contains(text(),'CHECK IN')]")).click();
+	    Thread.sleep(5000);
+	    }
+	    
+	    @Test(priority=5)
+	    public void staffcheckout() throws InterruptedException {
+	    driver.findElement(By.xpath("//a[@href='#/staff/staff-out']")).click();
+	    driver.findElement(By.xpath("//button[@type='button'][contains(text(),'CHECK OUT')]")).click();
+	    Thread.sleep(5000);
+	    }
+	    
+     @AfterTest
+     public void closeDriver() {
 		if(driver!=null) {
 			driver.close();
 		}
-	}	
+	}
 }
